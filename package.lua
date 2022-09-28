4c9d7af09345ba30653989035afb277d1b4fc177
console.log(cat == cute) <qwreey75@gmail.com>
2022-09-29 02:34:12 +0900

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "luaiod/random.lua"
+  version = "2.1.0"
+  dependencies = {}
+  license = "MIT"
+  homepage = "https://github.com/qwreey75/random.lua"
+  description = ""
+  tags = {}
+}
