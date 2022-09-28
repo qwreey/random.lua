14c226c61000b366a2ada732fb6173068ffe63ef
console.log(cat == cute) <qwreey75@gmail.com>
2022-09-29 02:43:27 +0900

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,9 +1,9 @@
 return {
-  name = "luaiod/random.lua"
-  version = "2.1.0"
-  dependencies = {}
-  license = "MIT"
-  homepage = "https://github.com/qwreey75/random.lua"
-  description = ""
-  tags = {}
+  name = "luaiod/random.lua",
+  version = "2.1.0",
+  dependencies = {},
+  license = "MIT",
+  homepage = "https://github.com/qwreey75/random.lua",
+  description = "",
+  tags = {},
 }
