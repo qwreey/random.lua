e63667ba4093b70effc07bbca4429f5ece504554
qwreey <me@qwreey.moe>
2023-12-28 00:58:20 +0000

    Update package information

--- package.lua
+++ package.lua
@@ -1,9 +1,16 @@
-return {
-  name = "luaiod/random.lua",
-  version = "2.1.0",
-  dependencies = {},
-  license = "MIT",
-  homepage = "https://github.com/qwreey75/random.lua",
-  description = "",
-  tags = {},
-}
+  return {
+    name = "qwreey/random",
+    version = "2.1.1",
+    description = "Random related functions",
+    tags = { "lua", "random", "math" },
+    license = "MIT",
+    author = { name = "qwreey", email = "me@qwreey.moe" },
+    homepage = "https://github.com/qwreey/random.lua",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!README.md",
+      "!test*"
+    }
+  }
+  
